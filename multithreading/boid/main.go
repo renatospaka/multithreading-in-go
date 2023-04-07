1939bf7746c82a873b3ea79efbb0c9700134a79e
Renato Spakauskas <renatospaka@gmail.com>
2023-04-07 12:25:57 -0300

    feat: :necktie: applying acceleration to the boids

--- multithreading/boid/main.go
+++ multithreading/boid/main.go
@@ -10,11 +10,14 @@ import (
 const (
 	screenWidth, screenHight = 640, 360
 	boidCount                = 500
+	viewRadius               = 13
+	adjRate                  = 0.015
 )
 
 var (
-	green = color.RGBA{10, 255, 50, 255}
+	green   = color.RGBA{10, 255, 50, 255}
 	boids   [boidCount]*Boid
+	boidMap [screenWidth + 1][screenHight + 1]int
 )
 
 type Game struct{}
@@ -37,6 +40,12 @@ func (g *Game) Layout(_, _ int) (w, h int) {
 }
 
 func main() {
+	for i, row := range boidMap {
+		for j := range row {
+			boidMap[i][j] = -1
+		}
+	}
+
 	for i := 0; i < boidCount; i++ {
 		createBoid(i)
 	}
