374c8b25dde1cf0bccfdbce12df96e3a129d3892
Renato Spakauskas <renatospaka@gmail.com>
2023-04-08 17:11:32 -0300

    feat: :sparkles: polygons area calculator
    a simple algorithm to calculate polygons area

---
+++ multithreading/threadpool/shoelace.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"regexp"
+	"strconv"
+)
+
+type Point2D struct {
+	x int
+	y int
+}
+
+var (
+	r = regexp.MustCompile(`\((\d*),(\d*)\)`)
+)
+
+func findArea(pointsStr string) {
+	var points []Point2D
+
+	for _, p := range r.FindAllStringSubmatch(pointsStr, -1) {
+		x, _ := strconv.Atoi(p[1])
+		y, _ := strconv.Atoi(p[2])
+		points = append(points, Point2D{x, y})
+	}
+
+	area := 0.0
+	for i := 0; i < len(points); i++ {
+		a, b := points[i], points[(i + 1) % len(points)]
+		area += float64(a.x * b.y) - float64(a.y * b.x)
+	}
+	fmt.Println(math.Abs(area) / 2.0)
+}
+
+func main() {
+	line := "(4,10),(12,8),(10,3),(2,2),(7,5)"
+	findArea(line)
+}
+
